a058734b104a4f9e194500181d50f50553fd6e21
Juanfer <juanfernandopertuz@gmail.com>
2025-12-03 18:33:45 -0500

    feat(basename): added basename property

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/AI-PORTFOLIO/">
       <App />
     </BrowserRouter>
   </StrictMode>
